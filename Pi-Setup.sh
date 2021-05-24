0aa0ee9d2b6bb926920d791d0aecefa8ab10c2a9
Andrea Ognibene <66163681+andreaognibene@users.noreply.github.com>
2021-05-24 21:16:06 +0200

    Update Pi-Setup.sh

--- Pi-Setup.sh
+++ Pi-Setup.sh
@@ -39,21 +39,42 @@ do
     esac
 done
 
+echo "HOSTNAME: ${HOSTNAME}"
+echo "USER: ${USER}"
+echo "IP: ${IP}"
+echo "GATEWAY: ${GATEWAY}"
+echo "DNS: ${DNS}"
+
+echo
+read -p "Continue? (y/n)" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
 # Change hostname.
+echo "Changing host name to ${HOSTNAME}..."
 hostnamectl set-hostname ${HOSTNAME}
+echo "Done."
 
 # Add user and add to SUDO group.
+echo "Creating new user ${USER}..."
 adduser ${USER}
 usermod -aG sudo ${USER}
+echo "Done."
 
 # Basic UFW setup.
+echo "Setting up UFW..."
 ufw allow ssh
 ufw allow 80/tcp
 ufw allow 443/tcp
 ufw enable
 ufw logging on
+echo "Done."
 
 # Static IP setup.
+echo "Setting up static IP ${IP}..."
 wget https://raw.githubusercontent.com/andreaognibene/Pi-Setup-Script/main/01-netcfg.yaml -P /etc/netplan/
 sed -i "s|%IP%|${IP}|g" /etc/netplan/01-netcfg.yaml
 sed -i "s|%GATEWAY%|${GATEWAY}|g" /etc/netplan/01-netcfg.yaml
@@ -61,6 +82,15 @@ sed -i "s|%DNS%|${DNS}|g" /etc/netplan/01-netcfg.yaml
 wget https://raw.githubusercontent.com/andreaognibene/Pi-Setup-Script/main/99-disable-network-config.cfg -P /etc/cloud/cloud.cfg.d/
 rm /etc/netplan/50-cloud-init.yaml
 netplan apply
+echo "Done."
+
+echo
+read -p "Run APT update and upgrade? (y/n)" -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
 
 # Update APT repository.
 apt update && apt upgrade -y
